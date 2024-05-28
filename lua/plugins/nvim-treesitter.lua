b5c93b939de26ccc1cc1af129dc248244137e3ba
alex <alex-suarez@live.com>
2024-05-28 15:17:21 -0600

    fix: change typo ensure_install -> ensure_installed

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,46 +1,46 @@
 local config = function()
-  require("nvim-treesitter.configs").setup({
-    build = ":TSUpdate",
-    indent = {
-      enable = true,
-    },
-    autotag = {
-      enable = true,
-    },
-    event = {
-      "BufReadPre",
-      "BufNewFile",
-    },
-    ensure_install = {
-      "markdown",
-      "json",
-      "javascript",
-      "typescript",
-      "tsx",
-      "yaml",
-      "html",
-      "css",
-      "bash",
-      "lua",
-      "dockerfile",
-      "gitignore",
-      "python",
-      "c",
-      "cpp",
-      "vue",
-      "svelte",
-      "toml",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = true,
-    },
-  })
+	require("nvim-treesitter.configs").setup({
+		build = ":TSUpdate",
+		indent = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		ensure_installed = {
+			"markdown",
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"bash",
+			"lua",
+			"dockerfile",
+			"gitignore",
+			"python",
+			"c",
+			"cpp",
+			"vue",
+			"svelte",
+			"toml",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+	})
 end
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  config = config,
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	config = config,
 }
