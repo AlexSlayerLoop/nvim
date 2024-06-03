3a8797daebb9ba745ad308316f7ce2a1c772e1b3
alex <alex-suarez@live.com>
2024-06-03 09:33:56 -0600

    fix: wsl clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,3 +37,19 @@ opt.cursorline = true
 opt.pumheight = 10
 opt.sidescrolloff = 8
 opt.mouse = "a"
+opt.clipboard:append("unnamedplus")
+
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
+end
