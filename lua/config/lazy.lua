cb9fdcb10e433a8f4a37d3c2c3d1a83cfcddf6d9
alejandro suarez <alex@alejandros-MacBook-Pro.local>
2024-08-10 20:18:22 -0600

    fix: change init.lua file name for lazy.lua and change an error nvim-lspconfig.lua file

---
+++ lua/config/lazy.lua
@@ -0,0 +1,44 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("config.globals")
+require("config.options")
+require("config.autocmds")
+require("config.helpers")
+require("config.keymaps")
+
+
+local opts = {
+  defaults = { lazy = true },
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  rtp = {
+	disabled_plugins = {
+		"gzip",
+		"matchit",
+		"matchparen",
+		"netrwPlugin",
+		"talPlugin",
+		"tohtml",
+		"tutor",
+		"zipPlugin",
+	},
+	}
+}
+
+-- Setup lazy.nvim
+require("lazy").setup("plugins", opts)
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -25,8 +25,8 @@ local config = function()
 				workspace = {
 					-- make language server aware of runtime files
 					library = {
-						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-						[vim.fn.stdpath("config") .. "/lua"] = true,
+            vim.fn.expand("$VIMRUNTIME/lua"),
+            vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
 					},
 				},
 				hint = { enable = true },
