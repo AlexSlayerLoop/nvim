03c0024a86818cf73c71869c9fdad9aeb61cfdf7
alejandro <alex-suarez@live.com>
2024-09-21 09:57:18 -0600

    fix: change lualine separators due to visual conflicts with transparent mode

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -3,8 +3,10 @@ local config = function()
 		options = {
 			theme = "auto",
 			globalstatus = true,
-			section_separators = { left = "", right = "" },
-			component_separators = { left = "", right = "" },
+			-- section_separators = { left = "", right = "" },
+			-- component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
 		},
 		sections = {
 			lualine_a = { "mode" },
