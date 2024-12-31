1b710bcb91211e1564ef43ffdccaecf01bba4a58
alejandro <alex-suarez@live.com>
2024-12-30 20:12:59 -0600

    feat: add new plugin for a dashboard in VimEnter

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,46 @@
+return {
+	"goolord/alpha-nvim",
+	enabled = true,
+	lazy = false,
+	depencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local alpha = require("alpha")
+		local theta = require("alpha.themes.theta")
+		local dashboard = require("alpha.themes.dashboard")
+
+		-- Set header
+		theta.header.val = {
+			[[                                   ]],
+			[[   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ]],
+			[[    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ]],
+			[[          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ]],
+			[[           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ]],
+			[[          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ]],
+			[[   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ]],
+			[[  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ]],
+			[[ ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ]],
+			[[ ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ]],
+			[[      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ]],
+			[[       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ]],
+			[[         Welcome to NeoVim         ]],
+		}
+
+		-- Set menu options
+		theta.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+			dashboard.button("g", "󱑓  Find Text", ":Telescope live_grep<CR>"),
+			dashboard.button("r", "󱑓  Recent files", ":Telescope oldfiles<CR>"),
+			dashboard.button("c", "  Config", function()
+				require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+			end),
+			dashboard.button("m", "󰇚  Mason", ":Mason<CR>"),
+			dashboard.button("l", "󰒲  Lazy", ":Lazy<CR>"),
+			dashboard.button("q", "󰍃  Quit", ":qa<CR>"),
+		}
+
+		alpha.setup(theta.config)
+	end,
+}
