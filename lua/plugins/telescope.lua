8df68161c8d0dd1f2088571f96b7da1f64fcdbfe
alejandro <alex-suarez@live.com>
2024-12-30 22:15:28 -0600

    refactor: change vim.lsp.buf builtins of lspsaga for Telescope and refactor the keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,14 @@
-local keys = {
-	{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Buffer search" },
-	{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
-	{ "<leader>fc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
-	{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find All Files" },
-	{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "Old Files" },
-	{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Ripgrep" },
-	{ "<leader>fs", "<cmd>Telescope grep_string<CR>", desc = "Grep String" },
-}
-
 local config = function()
 	local telescope = require("telescope")
 	telescope.setup({
-		defaults = {
+		defaults = require("telescope.themes").get_ivy({
 			mappings = {
 				i = {
 					["<C-j>"] = "move_selection_next",
 					["<C-k>"] = "move_selection_previous",
 				},
 			},
-		},
+		}),
 		pickers = {
 			live_grep = {
 				file_ignore_patterns = { "node_modules", ".venv", ".git" },
@@ -38,20 +28,111 @@ local config = function()
 			},
 		},
 		extensions = {
-			"fzf",
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				-- the default case_mode is "smart_case"
+			},
+			["ui-select"] = {
+				require("telescope.themes").get_dropdown({
+					previewer = false,
+					initial_mode = "normal",
+				}),
+			},
 		},
 	})
 	telescope.load_extension("fzf")
+	telescope.load_extension("ui-select")
 end
+
+local keys = {
+	{
+		"<leader>ff",
+		require("telescope.builtin").find_files,
+		desc = "[F]ind All [F]iles",
+	},
+	{
+		"<leader>fh",
+		require("telescope.builtin").help_tags,
+		desc = "[F]ind [H]elp",
+	},
+	{
+		"<leader>fb",
+		require("telescope.builtin").buffers,
+		desc = "[B]uffers",
+	},
+	{
+		"<leader>fc",
+		require("telescope.builtin").git_commits,
+		desc = "[C]ommits",
+	},
+	{
+		"<leader>fo",
+		require("telescope.builtin").oldfiles,
+		desc = "[O]ld Files",
+	},
+	{
+		"<leader>fr",
+		require("telescope.builtin").live_grep,
+		desc = "[R]ipgrep",
+	},
+	{
+		"<leader>fs",
+		require("telescope.builtin").grep_string,
+		desc = "Grep [S]tring",
+	},
+	{
+		"<leader>fa",
+		function()
+			require("telescope.builtin").builtin(require("telescope.themes").get_dropdown({ previewer = false }))
+		end,
+		desc = "Find [A]ll Telescopes",
+	},
+	{
+		"<leader>/",
+		function()
+			require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end,
+		desc = "[/] Fuzzily search in current buffer",
+	},
+	{
+		"<leader>s/",
+		function()
+			require("telescope.builtin").live_grep({ grep_open_files = true, prompt_title = "Live Grep in Open Files" })
+		end,
+		desc = "[S]earch [/] in Open Files",
+	},
+	{
+		"<leader>fn",
+		function()
+			require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+		end,
+		desc = "Search [N]eovim files",
+	},
+	{
+		"<leader>fp",
+		function()
+			require("telescope.builtin").find_files({
+				---@diagnostic disable-next-line: param-type-mismatch
+				cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+			})
+		end,
+		desc = "[P]lugins Data",
+	},
+}
 
 return {
 	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-		},
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 	},
 	config = config,
 	keys = keys,
--- lua/util/lsp.lua
+++ lua/util/lsp.lua
@@ -3,24 +3,56 @@ local keymap = vim.keymap
 local M = {}
 
 -- set keymaps on the active lsp server
-M.on_attach = function(client, bufnr)
-	local opts = { noremap = true, silent = true, buffer = bufnr }
+M.on_attach = function(_, bufnr)
+	-- local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	keymap.set("n", "<leader>fd", "<cmd>Lspsaga finder<CR>", opts) --find definition
-	keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>", opts) --peek definition
-	keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_definition<CR>", opts) --go to definition
-	keymap.set("n", "<leader>gS", "<cmd>Lspsaga vsplit | Lspsaga goto_definition<CR>", opts) --go to definition
-	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- show available code actions
-	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) --smart rename
-	keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) --show diagnostics for line
-	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) --show diagnostics for cursor
-	keymap.set("n", "<leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) --jump to prev diagnostic in ...
-	keymap.set("n", "<leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) --jump to next diagnostic in ...
-	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) --show documentation for what is under cursor
+	-- TODO: create this functionality
+	-- if client.name == "pyright" then
+	-- 	keymap.set("n", "<Leader>oi", "<cmd>PyrightOrganizeImports<CR>", opts)
+	-- end
 
-	if client.name == "pyright" then
-		keymap.set("n", "<Leader>oi", "<cmd>PyrightOrganizeImports<CR>", opts)
+	local map = function(keys, func, desc, mode)
+		mode = mode or "n"
+		keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
 	end
+
+	--  This is where a variable was first declared, or where a function is defined, etc.
+	--  To jump back, press <C-t>.
+	map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+	-- Find references for the word under your cursor.
+	map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+
+	-- Jump to the implementation of the word under your cursor.
+	--  Useful when your language has ways of declaring types without an actual implementation.
+	map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+	-- Jump to the type of the word under your cursor.
+	--  Useful when you're not sure what type a variable is and you want to see
+	--  the definition of its *type*, not where it was *defined*.
+	map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+
+	-- Fuzzy find all the symbols in your current document.
+	--  Symbols are things like variables, functions, types, etc.
+	map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+	-- Fuzzy find all the symbols in your current workspace.
+	--  Similar to document symbols, except searches over your entire project.
+	map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+	-- Rename the variable under your cursor.
+	--  Most Language Servers support renaming across files, etc.
+	map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+	-- Execute a code action, usually your cursor needs to be on top of an error
+	-- or a suggestion from your LSP for this to activate.
+	map("<leader>ca", function()
+		vim.lsp.buf.code_action({
+			handler = require("telescope.themes"),
+		})
+	end, "[C]ode [A]ction", { "n", "x" })
+
+	map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 end
 
 return M
