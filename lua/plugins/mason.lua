418bc294b645e0f8bb3810886ef1b88f41aadce5
alejandro <alex-suarez@live.com>
2024-12-30 20:48:35 -0600

    refactor: delete the mason-lspconfig file to add it to the mason file

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,56 @@
 return {
-	"williamboman/mason.nvim",
-	cmd = "Mason",
-	event = "BufReadPre",
-	opts = {
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		event = "BufReadPre",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
 			},
 		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"efm",
+				"lua_ls",
+				"pyright",
+				"bashls",
+				"ts_ls",
+				"emmet_ls",
+				"jsonls",
+				"dockerls",
+				"clangd",
+			},
+			automatic_installation = true,
+		},
+		event = "BufReadPre",
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = false,
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					"luacheck",
+					"black",
+					"flake8",
+					"clang-format",
+					"cpplint",
+					"eslint_d",
+					"prettierd",
+					"shfmt",
+					"shellcheck",
+					"fixjson",
+					"hadolint",
+				},
+			})
+		end,
 	},
 }
