c34b77419d8b3ac45b8b78525ea10fa7465e9310
alejandro <alex-suarez@live.com>
2025-02-14 23:15:49 -0600

    feat: avoid LspAttach for .env file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,20 +8,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-	callback = function(event)
-		local map = function(keys, func, desc, mode)
-			mode = mode or "n"
-			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-		end
-
-		local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-			map("<leader>uh", function()
-				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-			end, "[T]oggle Inlay [H]ints")
-		end
+vim.api.nvim_create_augroup("env", { clear = true })
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+	callback = function()
+		vim.lsp.stop_client(vim.lsp.get_clients())
 	end,
+	group = "env",
+	pattern = ".env",
+	desc = "LspDettach bashls for .env* files",
 })
