05a2b1564d6ff0f3830f35977836f49e038f791f
alejandro <alex-suarez@live.com>
2025-02-14 23:13:32 -0600

    feat: add lazydev for better completion in .config/nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,19 @@
 return {
 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	"folke/neodev.nvim",
+	{
+		"folke/lazydev.nvim",
+		lazy = false,
+		ft = "lua",
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				"lazy.nvim",
+				"snacks.nvim",
+				-- "oil.nvim",
+				-- "telescope.nvim",
+			},
+		},
+	},
 }
